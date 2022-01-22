5fe077715eec56b177f29a9d83ecc82cfbff459b
Giu_ <88463562+GiuDeMelo@users.noreply.github.com>
2022-01-22 11:45:42 -0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -2,14 +2,29 @@ const accessCode1 = "VARIABLE";
 const accessCode2 = "FUNCTION";
 const accessCode3 = "DATABASE";
 
-function clues() {
-    
-    fill("white")
-    textSize(15)
-    text("R E V B A I L A", 100,50)
-    fill("lightblue")
-    text("Dica: Sempre mudando, não constante!!", 100,70)
-// adicione código para exibir o restante dos nomes e as dicas.
-    
-
-}
+function clues(){
+    //CÓDIGO
+     fill("white");
+     textSize(15);
+   
+     //Code 1
+      text("R E V B A I L A", 100,50);
+   
+     //Code 2
+      text("C U T N I F O N", 700,150);
+   
+     //Code 3
+      text("A T E D A S B A", 100,250);
+   
+    //DICA
+     fill("lightblue");
+   
+     //Code 1
+      text("Dica: Sempre mudando, não constante", 100,70);
+   
+     //Code 2
+      text("Dica: Executa uma tarefa determinada",700,170);
+   
+     //Code 3
+      text("Dica: Armazena todas as informações",100,270);
+   }
